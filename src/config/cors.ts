957896bebace155d331fabe079c9bfd8d23886d1
fsilva <fsilva@urbano.com.pe>
2025-01-07 15:02:33 -0500

    second commit

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,13 +3,14 @@ import {CorsOptions} from 'cors'
 export const corsConfig:CorsOptions={
     origin:function(origin,callback){
         const whiteList=[
-            process.env.FRONTEND_URL
+            process.env.FRONTEND_URL,
+            process.env.BACKEND_URL
         ]
         if(process.argv[2]==='--api'){
             whiteList.push(undefined)
         }
 
-        if(whiteList.includes(origin)){
+        if(whiteList.includes(origin)|| !origin){
             callback(null,true)
         }else{
             callback(new Error('Error de CORS'))
